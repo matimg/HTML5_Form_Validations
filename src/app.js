0334a37cec1708e4f5a6127758d12dfc6a00e0f2
Matías Gandini <matias.gandinimg@gmail.com>
2021-04-22 14:43:04 +0000

    Correcion de bug

--- src/app.js
+++ src/app.js
@@ -24,69 +24,67 @@ window.onload = function() {
     let state = document.getElementById("state");
     let postal = document.getElementById("postal");
     let message = document.getElementById("message");
-    console.log(card);
+
     //Validaciones
-    if (validator.isEmpty(card.value)) {
+    if (
+      validator.isEmpty(card.value) ||
+      validator.isEmpty(cvc.value) ||
+      validator.isEmpty(amount.value) ||
+      validator.isEmpty(name.value) ||
+      validator.isEmpty(lastname.value) ||
+      validator.isEmpty(city.value) ||
+      validator.isEmpty(state.value) ||
+      validator.isEmpty(postal.value) ||
+      validator.isEmpty(message.value)
+    ) {
       alert.style.display = "block";
-      card.classList.add("alert-danger");
     } else {
       alert.style.display = "none";
+    }
+
+    if (validator.isEmpty(card.value)) {
+      card.classList.add("alert-danger");
+    } else {
       card.classList.remove("alert-danger");
     }
     if (validator.isEmpty(cvc.value)) {
-      alert.style.display = "block";
       cvc.classList.add("alert-danger");
     } else {
-      alert.style.display = "none";
       cvc.classList.remove("alert-danger");
     }
     if (validator.isEmpty(amount.value)) {
-      alert.style.display = "block";
       amount.classList.add("alert-danger");
     } else {
-      alert.style.display = "none";
       amount.classList.remove("alert-danger");
     }
     if (validator.isEmpty(name.value)) {
-      alert.style.display = "block";
       name.classList.add("alert-danger");
     } else {
-      alert.style.display = "none";
       name.classList.remove("alert-danger");
     }
     if (validator.isEmpty(lastname.value)) {
-      alert.style.display = "block";
       lastname.classList.add("alert-danger");
     } else {
-      alert.style.display = "none";
       lastname.classList.remove("alert-danger");
     }
     if (validator.isEmpty(city.value)) {
-      alert.style.display = "block";
       city.classList.add("alert-danger");
     } else {
-      alert.style.display = "none";
       city.classList.remove("alert-danger");
     }
     if (validator.isEmpty(state.value)) {
-      alert.style.display = "block";
       state.classList.add("alert-danger");
     } else {
-      alert.style.display = "none";
       state.classList.remove("alert-danger");
     }
     if (validator.isEmpty(postal.value)) {
-      alert.style.display = "block";
       postal.classList.add("alert-danger");
     } else {
-      alert.style.display = "none";
       postal.classList.remove("alert-danger");
     }
     if (validator.isEmpty(message.value)) {
-      alert.style.display = "block";
       message.classList.add("alert-danger");
     } else {
-      alert.style.display = "none";
       message.classList.remove("alert-danger");
     }
   });
